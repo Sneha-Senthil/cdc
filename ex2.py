61c5575416ed4b73ff099d03fe4de2807e035b54
Sneha Senthil <145248775+Sneha-Senthil@users.noreply.github.com>
2025-09-19 11:04:04 +0530

    Update ex2.py

--- ex2.py
+++ ex2.py
@@ -1,40 +1,54 @@
-Calc.py:
+# calc.py:
 import Pyro4
+
 @Pyro4.expose
 class Calculator:
-def add(self, num1, num2):
-return num1 + num2
-def sub(self, num1, num2):
-return num1 - num2
-def mul(self, num1, num2):
-return num1 * num2
-def div(self, num1, num2):
-try:
-return num1 / num2
-except:
-return "Cant Divide by Zero..."
+    def add(self, num1, num2):
+        return num1 + num2
 
-Client.py:
-import Pyro4
-name_server = Pyro4.locateNS()
-calc_uri = name_server.lookup("ex2.calculator")
-# Get the RMI object
-calc_rmi = Pyro4.Proxy(calc_uri)
-num1 = 10
-num2 = 12
-print("Two Numbers are: {}, {}".format(num1, num2))
-print(f"Add: {calc_rmi.add(num1, num2)}")
-print(f"Subtract: {calc_rmi.sub(num1, num2)}")
-print(f"Multiply: {calc_rmi.mul(num1, num2)}")
-print(f"Divide: {calc_rmi.div(num1, num2)}")
+    def sub(self, a, b):
+        return a - b
 
-Server.py:
+    def mul(self, a, b):
+        return a * b
+    
+    def div(self, a, b):
+        try:
+            return a / b
+        except:
+            return "Can't divide by Zero.."
+        
+# server.py:
 import Pyro4
 from calc import Calculator
+
 daemon = Pyro4.Daemon()
 calc_uri = daemon.register(Calculator)
-# Use a name server to regsiter for a namespace.
+
 name_server = Pyro4.locateNS()
 name_server.register("ex2.calculator", calc_uri)
-print(f"<SERVER> Calculator for RMI Accessible via 'ex2.calculator' or {calc_uri}")
+
+print(f"<SERVER> Calculator for RMI Accesible via 'ex2.calculator' or {calc_uri}")
+
 daemon.requestLoop()
+
+# client.py:
+import Pyro4
+
+name_server = Pyro4.locateNS()
+
+calc_uri = name_server.lookup("ex2.calculator")
+
+# Get the RMI Obj
+calc_rmi = Pyro4.Proxy(calc_uri)
+
+a = 10
+b = 12
+
+print(f"Two numbers are: {a}, {b}")
+
+print(f"Add: {calc_rmi.add(a, b)}")
+print(f"Sub: {calc_rmi.sub(a, b)}")
+print(f"Mul: {calc_rmi.mul(a, b)}")
+print(f"Div: {calc_rmi.div(a, b)}")
+	
